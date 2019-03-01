489fca911a623b0d386e144a1f3a115656c4ff93
[Tim] <ryan.timothypatrick@gmail.com>
2019-02-28 23:39:37 -0800

    Second Commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,12 @@
 #######**PyPOLL**#######
+
+ Modules
+import os
+import csv
+
+# Set path for file
+csvpath = os.path.join("..", "Resources", "election_data.csv")
+
+# Open the CSV
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
